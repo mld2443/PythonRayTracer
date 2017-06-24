27203ccf866aba2158b06d85b946c2d55168b47a
Matthew Dillard <7395051@gmail.com>
2017-06-24 12:05:39 -0500

    Adding notes as I go
    Proper documentation is what separates humanity from animals.

--- Camera.py
+++ Camera.py
@@ -30,11 +30,13 @@ def capture(camera, scene, sampling, depth):
     # create the empty pixel array to convert to an image
     pixels = []
 
+    # Build the image one pixel at a time
     for y in range(camera.height)
         for x in range(camera.width)
             pixel_position = top_left + x * deltaX - y * deltaY
             pixels[y][x] = get_pixel(scene, pixel_position, (deltaY, deltaY), depth)
 
+    # Convert our array from what is essentially a bitmap to an image
     return image_from_pixels(pixels, (camera.width, camera.height))
 
 def get_pixel(scene, position, samples, pixel_size, depth):
@@ -53,20 +55,39 @@ def get_pixel(scene, position, samples, pixel_size, depth):
 
     # Color correction
     pixel = pixel / samples
+    #TODO: check how this looks with transform
     #pixel = pixel.apply_transform(sqrt)
 
+    # This converts from our floating point to 8bit colorspace.
+    # I do this by clipping any value greater than the max.
+    # Another way to do this is to normalize all values with
+    # the largest all at once after calculating all the pixels.
     return pixel.quantize()
 
 def trace(ray, scene, depth):
+    # Base case; try changing the color and seeing what you get!
     return black if depth <= 0
 
+    # check to see if our ray hits an object, or just shoots into space
     intersect = cast_ray(scene, ray, frustrum)
     if intersect:
+        # Get the color of that object and the bounce vector for recursion if there is recursion
         color, bounce = intersect.material.scatter(ray, intersect, scene_refraction_index)
 
+        #TODO: check how this looks with color
+        # Here is the actual color blending; it's very simple
         return color * trace(bounce, scene, depth - 1) if bounce else black
     else:
+        return sky_gradient(ray.direction)
+
+################
+# Sky gradient #
+################
+# It's important this be bright, as it's the
+# source of much of the light of the scene,
+# Otherwise it's just for looks.
 def sky_gradient(direction):
+    #TODO: Check how this looks with curve correction
     interpolate = (0.5 * (direction.y + 1)) #**0.5
     return white * (1 - interpolate) + Color(0.5, 0.7, 1.0) * interpolate
 
